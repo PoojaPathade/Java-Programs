6de4bf2d2d52055fd80345c4f85cb28db39a168b
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-05-21 14:14:14 +0530

    Create 8_Iteration3.java
    use do while loop

---
+++ 8_Iteration3.java
@@ -0,0 +1,21 @@
+
+class Iteration2
+{
+    public static void main(String Arg[])
+    {
+        int No = 0;
+
+        No = 1;
+
+        do
+        {
+            System.out.println("Hello World");
+            No++;
+        }
+
+        
+        while (No<=4);
+
+    }
+    
+}
