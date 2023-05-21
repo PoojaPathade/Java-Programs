eb1df495d02fbcbb96c39f54eb23a116de8df6f8
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-05-21 13:56:33 +0530

    Create Iteration1.java
    For Loop

---
+++ Iteration1.java
@@ -0,0 +1,11 @@
+class Iteration
+{
+    public static void main(String Arg[])
+    {
+        int Cnt = 0 ;
+        for(Cnt = 1; Cnt <=4; Cnt++ )
+        {
+            System.out.println("Hello World....");
+        }
+    }
+}
