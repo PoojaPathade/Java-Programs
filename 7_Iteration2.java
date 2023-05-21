d1605bc9684f2fbcd5bda4cbee11612cbb5491e9
PoojaPathade <133394776+PoojaPathade@users.noreply.github.com>
2023-05-21 14:07:00 +0530

    Create 7_Iteration2.java
    use while loop

---
+++ 7_Iteration2.java
@@ -0,0 +1,18 @@
+
+class Iteration2
+{
+    public static void main(String Arg[])
+    {
+        int No = 0;
+
+        No = 1;
+
+        while (No<=4)
+        {
+            System.out.println("Hello World");
+            No++;
+        }
+
+    }
+    
+}
